c492b895d97f72b888758215975662c3ddfe78a4
Pascal <phaenni01@gmail.com>
2026-08-27 15:24:46 +0200

    Day 1 Level 2

--- Years/2025/Day01/Program.cs
+++ Years/2025/Day01/Program.cs
@@ -4,24 +4,18 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        const string examplePath = @".\data\example.txt";
-        const string inputPath = @".\data\input.txt";
-
-        int dial = 50;
+        const string path = @".\data\input.txt";
+        var list = getFile(path);
         
-        var list = getFile(inputPath);
-
+        var dial = 50;
         var zeroCounter = 0;
-        
+
         foreach (var line in list)
         {
-            dial = moveDial(line[0].ToString(), Convert.ToInt32(line.Substring(1)), dial);
-            
-            if (dial== 0)
-            {
-                zeroCounter++;
-            }
+            (var zeros, dial) = moveDial(line[0].ToString(), Convert.ToInt32(line.Substring(1)), dial);
+            zeroCounter += zeros;
         }
+
         Console.WriteLine(zeroCounter);
     }
 
@@ -30,27 +24,56 @@ internal class Program
         return File.ReadAllLines(path).ToList();
     }
 
-    static int moveDial(string direction, int count, int dial)
+    static int checkNum(int dial)
     {
-        if (direction.Trim() == "L")
+        while (dial < 0)
         {
-            dial -= count;
+            dial += 100;
         }
 
-        if (direction.Trim() == "R")
-        {
-            dial += count;
-        }
-        
         while (dial > 99)
         {
             dial -= 100;
         }
 
-        while (dial < 0)
-        {
-            dial += 100;
-        }
         return dial;
+    }
+    static (int, int) moveDial(string direction, int count, int dial)
+    {
+        var zeroCounter = 0;
+
+        switch (direction.Trim())
+        {
+            case "R":
+                for (int i = 0; i < count; i++)
+                {
+                    dial = checkNum(dial);
+                    if (dial == 0)
+                    {
+                        zeroCounter++;
+                    }
+
+                    dial++;
+                }
+
+                break;
+
+            case "L":
+                for (int i = 0; i < count; i++)
+                {
+                    dial = checkNum(dial);
+                    if (dial == 0)
+                    {
+                        zeroCounter++;
+                    }
+
+                    dial--;
+                }
+
+                break;
+        }
+        Console.WriteLine(dial);
+
+        return (zeroCounter, dial);
     }
 }
