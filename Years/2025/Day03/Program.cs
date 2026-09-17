432df2ace63561020100fb0783a3e3d6bc63b2e6
Pascal <phaenni01@gmail.com>
2026-09-17 08:28:53 +0200

    generate day 3 and fill data

---
+++ Years/2025/Day03/Program.cs
@@ -0,0 +1,23 @@
+namespace Y2025.Day03;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Advent of Code 2025 - Day 03");
+
+        
+        System.Console.WriteLine("Part 1:" + Part1(input));
+        System.Console.WriteLine("Part 2:" + Part2(input));
+    }
+
+    static string Part1(string[] input)
+    {
+        return string.Join('\n', input);
+    }
+
+    static string Part2(string[] input)
+    {
+        return string.Join('\n', input);
+    }
+}
