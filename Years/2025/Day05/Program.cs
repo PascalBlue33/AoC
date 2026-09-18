43d0e90f5283d2b33fdac96a3b56c963af068bfb
Pascal Hänni BIT <pascal.haenni@bit.admin.ch>
2026-09-18 15:17:54 +0200

    day 5 and data

---
+++ Years/2025/Day05/Program.cs
@@ -0,0 +1,26 @@
+namespace Y2025.Day05;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = $"./data/input.txt";
+
+        var list = File.ReadAllLines(path);
+
+        foreach (string line in list)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    static string read(string[] input)
+    {
+        return string.Join('\n', input);
+    }
+
+    static string Part2(string[] input)
+    {
+        return string.Join('\n', input);
+    }
+}
